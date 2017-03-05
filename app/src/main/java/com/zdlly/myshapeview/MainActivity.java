d6437b934fc048204c53266d717d5aade91e4691
Harry Ye <zdllyzs@outlook.com>
2017-03-05 21:42:13 +0800

    完成了一部分功能，但在多边形imageview上总会有一个bug

--- app/src/main/java/com/zdlly/myshapeview/MainActivity.java
+++ app/src/main/java/com/zdlly/myshapeview/MainActivity.java
@@ -1,13 +1,54 @@
 package com.zdlly.myshapeview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edit_sides;
+    private Button submit;
+    private MyShapeView my_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        edit_sides = (EditText) findViewById(R.id.edit_sides);
+        submit = (Button) findViewById(R.id.submit);
+        my_view = (MyShapeView) findViewById(R.id.my_view);
+        my_view.setImageDrawable(getDrawable(R.mipmap.ic_launcher));
+        submit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.submit:
+                submit();
+                break;
+        }
+    }
+
+    private void submit() {
+        // validate
+        String sides = edit_sides.getText().toString().trim();
+        if (TextUtils.isEmpty(sides)) {
+            Toast.makeText(this, "sides不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        my_view.setmSides(Integer.parseInt(sides));
+        my_view.setImageDrawable(getDrawable(R.drawable.myjpg));
+        my_view.invalidate();
+
+
     }
 }
---
+++ app/src/main/java/com/zdlly/myshapeview/MyShapeView.java
@@ -0,0 +1,110 @@
+package com.zdlly.myshapeview;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Xfermode;
+import android.util.AttributeSet;
+import android.widget.Toast;
+
+/**
+ * Created by zdlly on 2017/3/5.
+ */
+public class MyShapeView extends android.support.v7.widget.AppCompatImageView {
+
+
+    private int mSides = 3;
+    private Paint mPaint;
+    private Xfermode mXfermode;
+    private Bitmap mMask;
+    private Path path;
+    private Canvas canvas;
+
+
+    public void setmSides(int mSides) {
+        this.mSides = mSides;
+    }
+
+    public MyShapeView(Context context) {
+        this(context, null);
+    }
+
+    public MyShapeView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MyShapeView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.WHITE);
+        mXfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
+        path = new Path();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        int id = canvas.saveLayer(0, 0, canvas.getWidth(), canvas.getHeight(), null, Canvas.ALL_SAVE_FLAG);
+        createMask();
+        super.onDraw(canvas);
+        mPaint.setXfermode(mXfermode);
+        canvas.drawBitmap(mMask, 0, 0, mPaint);
+        mPaint.setXfermode(null);
+        canvas.restoreToCount(id);
+
+
+    }
+
+
+    private void createMask() {
+
+        int maskWidth = getMeasuredWidth();
+        int maskHeight = getMeasuredHeight();
+        mMask = Bitmap.createBitmap(maskWidth, maskHeight, Bitmap.Config.ALPHA_8);
+
+        canvas= new Canvas(mMask);
+        int rectSize = maskWidth / 2;
+        drawMultShape(canvas, mSides, rectSize);
+
+    }
+
+    public void drawMultShape(Canvas canvas, int count, float radius) {
+
+        canvas.translate(radius, radius);
+        if (count < 3) {
+            Toast.makeText(getContext(), "无法绘制边数少于3的图形", Toast.LENGTH_SHORT).show();
+        } else if (count < 20) {
+            for (int i = 0; i < count; i++) {
+                if (i == 0) {
+                    path.moveTo(radius * cos(360 / count * i), radius * sin(360 / count * i));
+                } else {
+                    path.lineTo(radius * cos(360 / count * i), radius * sin(360 / count * i));
+                }
+            }
+            path.close();
+            canvas.drawPath(path, mPaint);
+        } else {
+            canvas.drawCircle(0, 0, radius, mPaint);
+        }
+    }
+
+
+    float sin(int num) {
+        return (float) Math.sin(num * Math.PI / 180);
+    }
+
+    float cos(int num) {
+        return (float) Math.cos(num * Math.PI / 180);
+    }
+}
